bff62d36d93ae9723d38c48f4a872a033b14ff74
Shahid Parvez <shahidparvez.official@gmail.com>
2022-08-04 22:16:50 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(wildcard $(LOCAL_DIR)/kasumi_rosemary.mk)
 
 COMMON_LUNCH_CHOICES := \
-    kasumi_rosemary-user \
-    kasumi_rosemary-userdebug \
-    kasumi_rosemary-eng
+    aosp_rosemary-user \
+    aosp_rosemary-userdebug \
+    aosp_rosemary-eng
